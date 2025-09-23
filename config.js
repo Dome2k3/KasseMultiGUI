6217b4fa93624e89595c81d9a123f30355e3f433
Dome2k3 <bucherdominik@web.de>
2025-09-23 21:44:16 +0200

    Update API_URL for production environment

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // window.API_URL = "http://192.168.0.187:3000"; // lokal
- window.API_URL = "https://george-thriller-administrators-heated.trycloudflare.com"; // für Produktion/ngrok
+ window.API_URL = "https://gorgeous-spatial-robert-traditions.trycloudflare.com"; // für Produktion/ngrok
