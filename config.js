8963b41e7b7b5a2590a8bf086bc121ab34c60822
Dome2k3 <bucherdominik@web.de>
2025-09-23 13:44:58 +0200

    Fix API_URL assignment in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // window.API_URL = "http://192.168.0.187:3000"; // lokal
- window.API_URL = " https://unmisinterpretable-overactive-rosana.ngrok-free.dev"; // für Produktion/ngrok
+ window.API_URL = "https://unmisinterpretable-overactive-rosana.ngrok-free.dev"; // für Produktion/ngrok
