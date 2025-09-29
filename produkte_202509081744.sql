2773d602cb09f516928553e76a46ae4f7ebe5dfc
Dome2k3 <bucherdominik@web.de>
2025-09-29 15:21:30 +0200

    Add files via upload

---
+++ produkte_202509081744.sql
@@ -0,0 +1,18 @@
+INSERT INTO volleyball_turnier.kasse_produkte (name,preis,kategorie) VALUES
+	 ('Wurst - Test',3.50,'Wurst', ),
+	 ('Wurst - Schwein',3.50,'Wurst'),
+	 ('Currywurst - Rind',4.00,'Wurst'),
+	 ('Currywurst - Schwein',4.00,'Wurst'),
+	 ('Steak',5.50,'Fleisch'),
+	 ('Gyros',4.50,'Fleisch'),
+	 ('Hamburger',6.00,'Fast Food'),
+	 ('Cheeseburger',6.50,'Fast Food'),
+	 ('Pommes',2.50,'Beilage'),
+	 ('Brötchen',0.50,'Beilage');
+INSERT INTO volleyball_turnier.kasse_produkte (name,preis,kategorie) VALUES
+	 ('Salat',5.50,'Vegetarisch'),
+	 ('Falafel',3.50,'Vegetarisch'),
+	 ('Pfand',1.00,'Pfand'),
+	 ('Pfand Rückgabe',-1.00,'Pfand'),
+	 ('Flammkuchen Speck',7.00,'Flammkuchen'),
+	 ('Flammkuchen Veg',6.50,'Flammkuchen');
