5bd470ec42a630015778f2a82c32dae9f4b5d7d6
Dome2k3 <bucherdominik@web.de>
2025-09-30 23:23:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 // window.API_URL = "http://192.168.0.187:3000"; // lokal
+// window.API_URL_TEAMS = "http://192.168.0.187:3002"; // TEAMS Seite
  window.API_URL = "https://meinraspi-tcp.lgrw.de"; // für Produktion/ngrok
+ window.API_URL_TEAMS = "https://meinraspi-tcp.lgrw.de"; //test ob es geht
