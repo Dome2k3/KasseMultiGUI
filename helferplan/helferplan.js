81ad33dedd4a7749d31519f1b6330ab57630c460
dominikbucher <bucherdominik@web.de>
2025-10-24 23:17:06 +0200

    V0.9 Druck ungetestet Helferplan Staffelpreise ergänzt

--- helferplan/helferplan.js
+++ helferplan/helferplan.js
@@ -16,9 +16,9 @@ const port = Number(process.env.PORT) || 3003;
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST || 'localhost',
     port: process.env.MYSQL_PORT || 3306,
-    user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || '',
-    database: process.env.MYSQL_DATABASE || 'volleyball_turnier',
+    user: process.env.MYSQL_USER || ,
+    password: process.env.MYSQL_PASSWORD || ,
+    database: process.env.MYSQL_DATABASE || ,
     waitForConnections: true,
     connectionLimit: Number(process.env.MYSQL_CONNECTION_LIMIT) || 5,
     timezone: 'Z'
