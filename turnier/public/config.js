3372ef0e3ed9ddce9fcd920b8e78d7fc3fd90595
dominikbucher <bucherdominik@web.de>
2025-12-04 22:07:13 +0100

    Turnier config.js

--- turnier/public/config.js
+++ turnier/public/config.js
@@ -1,5 +1,6 @@
 // runtime config for Helferplan - must be loaded BEFORE requests.js / plan.js
 window.__API_URL_TURNIER = 'https://meinraspi-tcp-turnier.lgrw.de/api';
 // optional expose for debugging
-window.__API_URL_TURNIER = window.__API_URL_TURNIER;
+window.__API_URL_TURNIER = window.API_URL_TURNIER || 'https://meinraspi-tcp-turnier.lgrw.de/api';
 console.info('[config.js] __API_URL_TURNIER =', window.__API_URL_TURNIER);
+
